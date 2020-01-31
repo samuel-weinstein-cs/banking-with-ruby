384feed665501b5e87e9a2d4ff43747894fb0721
sweinstein <raeweinst@gmail.com>
2020-01-31 15:59:34 -0500

    latest code

--- customer.rb
+++ customer.rb
@@ -15,14 +15,18 @@ class Customer
     @this_customer_id = @@next_customer_id
     @@next_customer_id += 1
     @this_accounts = []
+    save if !(first_name == nil || last_name == nil || ssn == nil || address == nil)
+
   end
 
   def retrieve_data(customer_id)
     file = File.open(File.dirname(__FILE__) + '/data/customer_table.txt', 'r')
-    if customer_id < @@next_customer_id
+    puts customer_id, @@next_customer_id
+    if customer_id+1 < @@next_customer_id
       match = nil
-      file.readlines.each do |line|
-        matches = line.match(/(\d+) (\w+) (\w+) (\d{3}-\d{2}-\d{4}) "(.+)" (\[.+\])/)
+      lines = file.readlines
+      lines.each do |line|
+        matches = line.match(/(\d+) (\w+) (\w+) (\d{3}-\d{2}-\d{4}) "(.+)" (\[.*\])/)
         next unless matches.captures[0].to_i == customer_id
 
         return {
@@ -57,26 +61,34 @@ class Customer
   end
   def save
     customer_data = retrieve_data(@this_customer_id)
+
+    puts customer_data
     file = customer_data['file']
     file.rewind
     lines = file.readlines
-    lines[customer_data['matches'][0].to_i-1] = "#{@this_customer_id} #{@this_first_name} #{@this_last_name} #{@this_ssn} \"#{@this_address}\" #{@this_accounts}"
-    file.close #sorry
-    file = File.open(File.dirname(__FILE__) + '/data/customer_table.txt', 'w')
-    file.puts(lines)
-
-
+    file.close
+    str = "#{@this_customer_id} #{@this_first_name} #{@this_last_name} #{@this_ssn} \"#{@this_address}\" #{@this_accounts}"
 
     # puts str
-    if customer_data
-
+    if customer_data['matches']
+      lines[customer_data['matches'][0].to_i-1] = str
       # print file.readlines
+    else
+      lines.push str
     end
+
+    file = File.open(File.dirname(__FILE__) + '/data/customer_table.txt', 'w') #sorry for ambiguity lmao
+    file.puts(lines)
+    file.close
   end
 
 end
-# c = Customer.new('Sam', 'Weinstein', '123-45-6789', '12345 Bruh Moment Drive, New York, New York')
-d = Customer.new
-d.find_by_id(1)
-d.this_first_name = "AAAAAAAAAAAAA"
+c = Customer.new('Bruh', 'Moment', '123-45-6789', '12345 Bruh Moment Drive, New York, New York')
+d = Customer.new('lasdkjflkasd', 'daskfj', '123-45-6789', '12345 Bruh Moment Drive, New York, New York')
 d.save
+c.this_first_name = "asdfghjkl"
+c.save
+# d = Customer.new
+# d.find_by_id(1)
+# d.this_first_name = "eeeeeesadljkfasd,ee"
+# d.save
